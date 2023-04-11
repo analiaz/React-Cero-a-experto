380f27e3cf3f09314afcc3a3077ee508aa4ac558
Analia Lujan Zunino <analia.zunino@accenture.com>
2023-04-11 16:13:24 -0300

    visualizar arreglo

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([ 'One Punch', 'Dragon ball']);
+
+    const onAddCategory = () => {
+        setCategories([ ...categories, 'sailor moon']);
+    }
+
   return (
     <>
        <h1>GifExpertApp</h1>
+        <button onClick={ onAddCategory }>Agregar</button>
+       <ol>
+            {categories.map( category => {
+                return <li key={ category }>{category}</li>
+            } )}
+       </ol>
     </>
   )
 }
