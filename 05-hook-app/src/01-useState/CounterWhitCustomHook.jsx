8d5d96d551334bcb725c720c844f4b72831992e2
Analia Lujan Zunino <analia.zunino@accenture.com>
2023-04-21 11:08:58 -0300

    useState act

---
+++ 05-hook-app/src/01-useState/CounterWhitCustomHook.jsx
@@ -0,0 +1,23 @@
+import { useCounter } from "../Hooks/useCounter"
+
+const CounterWhitCustomHook = () => {
+  
+    const { counter, increment, decrement, reset } = useCounter();
+  
+    return (
+    <>
+        <h1>Counter with Hook: { counter }</h1>
+        <hr />
+
+        <button onClick={ () => increment(1)} className="btn btn-primary"> +1 </button>
+        <button onClick={ reset } className="btn btn-primary"> Reset </button>
+        <button onClick={ () => decrement(1)} className="btn btn-primary"> -1 </button>
+        
+
+
+
+    </>
+  )
+}
+
+export default CounterWhitCustomHook
---
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,16 @@
+
+export default function SimpleForm() {
+  return (
+    <>
+        <h1>Formulario Simple</h1>
+        <hr />
+
+        <input 
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            name="username"
+         />
+    </>
+  )
+}
