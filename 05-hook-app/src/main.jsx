6eb42e7be270e6b8ac3b43166054d7b7a19132b0
Analia Lujan Zunino <analia.zunino@accenture.com>
2023-04-24 16:27:40 -0300

    memo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -7,12 +7,14 @@ import ReactDOM from 'react-dom/client';
 //import {FormWithCustomeHook} from './02-useEffect/FormWithCustomeHook'
 //import {MultipleCustomHooks} from "./03-examples/MultipleCustomHooks";
 import './index.css';
+//import { Memorize } from './06-memos/memorize';
+import { MemoHook } from './06-memos/MemoHook';
 
 //import { FocusScreen } from './04-useRef/FocusScreen';
-import { Layout } from './05-useLayoutEffect/Layout';
+//import { Layout } from './05-useLayoutEffect/Layout';
 
 ReactDOM.createRoot(document.getElementById('root') ).render(
   <React.StrictMode>
-    <Layout />
+    <MemoHook />
   </React.StrictMode>,
 )
